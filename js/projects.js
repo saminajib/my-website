fdc86d1dbebeaf4a2d2034874259451a09deb895
saminajib <saminajib2000@gmail.com>
2024-10-10 13:42:55 -0400

    fixed click for icons

--- js/projects.js
+++ js/projects.js
@@ -6,6 +6,27 @@ function openModal(name, description, website, github) {
     $("#modal").removeClass("hidden");
 }
 
+function studyFlash(){
+    openModal('PL/0 Compiler',
+        'Compiler for PL/0 language using top down parser descent. Guarantees adherence to language EBNF and generates corresponding machine code if no grammar or syntax errors exist. Repo also contains virtual machine to test machine code and simulate stack.',
+        '',
+        'https://github.com/saminajib/pl0-compiler');
+}
+
+function pl0Compiler(){
+    openModal('PL/0 Compiler',
+        'Compiler for PL/0 language using top down parser descent. Guarantees adherence to language EBNF and generates corresponding machine code if no grammar or syntax errors exist. Repo also contains virtual machine to test machine code and simulate stack.',
+        '',
+        'https://github.com/saminajib/pl0-compiler');
+}
+
+function contactManager() {
+    openModal('Contact Manager',
+        'A basic contact manager using LAMP stack which contains a user database as well as corresponding contacts.',
+        '',
+        'https://github.com/saminajib/Contact-Manager');
+}
+
 function exitModal(){
     $("#modal").addClass("hidden");
 }
