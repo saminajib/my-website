107ef8bac5337e50153d35ad444a791ec222d2b9
saminajib <saminajib2000@gmail.com>
2024-10-10 13:44:26 -0400

    fixed click for icons

--- js/projects.js
+++ js/projects.js
@@ -7,10 +7,10 @@ function openModal(name, description, website, github) {
 }
 
 function studyFlash(){
-    openModal('PL/0 Compiler',
-        'Compiler for PL/0 language using top down parser descent. Guarantees adherence to language EBNF and generates corresponding machine code if no grammar or syntax errors exist. Repo also contains virtual machine to test machine code and simulate stack.',
-        '',
-        'https://github.com/saminajib/pl0-compiler');
+    openModal('Study Flash',
+        'A website to make studying as efficient as possible by saving time with advanced ai study material generation.',
+        'https://studyflash.study',
+        'https://github.com/seb1124/studyFlash');
 }
 
 function pl0Compiler(){
